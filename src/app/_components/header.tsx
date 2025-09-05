75cda42943a49d9cdc735a4163972ad60e2503cc
Infinity <wemersoncontas22@gmail.com>
2025-09-05 13:06:52 -0300

    chore: add components from shadcn

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,16 +1,25 @@
-import { MenuIcon } from "lucide-react"
-import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+import { Sheet, SheetTrigger } from "./ui/sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
-        <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+        <Link href="/">
+          <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
+        </Link>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+        </Sheet>
       </CardContent>
     </Card>
   )
