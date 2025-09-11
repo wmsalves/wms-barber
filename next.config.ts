a81b0d55a293c323a0e8c4bf8418f65147eb95b5
Infinity <wemersoncontas22@gmail.com>
2025-09-11 10:07:17 -0300

    feat: add BarbershopItem component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
+  },
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,107 @@
-"use client"
-
 import { SearchIcon } from "lucide-react"
+
 import Header from "./_components/header"
+
 import { Button } from "./_components/ui/button"
+
 import { Input } from "./_components/ui/input"
+
 import Image from "next/image"
+
 import { Card, CardContent } from "./_components/ui/card"
+
 import { Badge } from "./_components/ui/badge"
+
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
-const Home = () => {
+import { db } from "./_lib/prisma"
+
+import BarbershopItem from "./_components/barbershop-item"
+
+const Home = async () => {
+  // chamar meu banco de dados
+
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
-      {/* Header */}
+      {/* header */}
+
       <Header />
+
       <div className="p-5">
         {/* TEXTO */}
-        <h2 className="text-xl font-bold">Olá, Wemerson</h2>
-        <p>Segunda-Feira, 05 de Setembro</p>
+
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+
+        <p>Segunda-feira, 05 de agosto.</p>
 
         {/* BUSCA */}
-        <div className="nt-6 flex items-center gap-2">
+
+        <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
+
           <Button>
             <SearchIcon />
           </Button>
         </div>
 
         {/* IMAGEM */}
-        <div className="relative h-[150px] w-full">
+
+        <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
             src="/banner-01.png"
             fill
-            className="object-cover"
+            className="rounded-xl object-cover"
           />
         </div>
 
         {/* AGENDAMENTO */}
-        <h2>Agendamentos</h2>
-        <Card className="mt-6">
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* ESQUERDA */}
+
             <div className="flex flex-col gap-2 py-5 pl-5">
               <Badge className="w-fit">Confirmado</Badge>
+
               <h3 className="font-semibold">Corte de Cabelo</h3>
 
               <div className="flex items-center gap-2">
                 <Avatar className="h-6 w-6">
                   <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
                 </Avatar>
+
                 <p className="text-sm">Barbearia FSW</p>
               </div>
             </div>
-            {/* ESQUERDA */}
-            <div className="-px-5 flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Setembro</p>
+
+            {/* DIREITA */}
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+
               <p className="text-2xl">05</p>
-              <p className="text-sm">14:00</p>
+
+              <p className="text-sm">20:00</p>
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
