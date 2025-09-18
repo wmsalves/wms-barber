9ea2b54f86c784cf887937e90ea1c25c4294a39e
Infinity <wemersoncontas22@gmail.com>
2025-09-18 08:43:19 -0300

    feat: ensure booking can be created

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,6 +18,18 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+
+        id: user.id,
+      } as any
+
+      return session
+    },
+  },
 })
 
 export { handler as GET, handler as POST }
