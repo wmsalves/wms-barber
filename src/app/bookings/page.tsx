f4f0de298c17cc051d3fcc332b540313d61f5074
Infinity <wemersoncontas22@gmail.com>
2025-09-20 07:34:18 -0300

    refactor: add data functions to get bookings

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -2,13 +2,15 @@ import { getServerSession } from "next-auth"
 
 import Header from "../_components/header"
 
-import { db } from "../_lib/prisma"
-
 import { authOptions } from "../_lib/auth"
 
 import { notFound } from "next/navigation"
 
 import BookingItem from "../_components/booking-item"
+
+import { getConfirmedBookings } from "../_data/get-confirmed-bookings"
+
+import { getConcludedBookings } from "../_data/get-concluded-bookings"
 
 const Bookings = async () => {
   const session = await getServerSession(authOptions)
@@ -19,49 +21,9 @@ const Bookings = async () => {
     return notFound()
   }
 
-  const confirmedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
+  const confirmedBookings = await getConfirmedBookings()
 
-      date: {
-        gte: new Date(),
-      },
-    },
-
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-
-    orderBy: {
-      date: "asc",
-    },
-  })
-
-  const concludedBookings = await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-
-      date: {
-        lt: new Date(),
-      },
-    },
-
-    include: {
-      service: {
-        include: {
-          barbershop: true,
-        },
-      },
-    },
-
-    orderBy: {
-      date: "asc",
-    },
-  })
+  const concludedBookings = await getConcludedBookings()
 
   return (
     <>
